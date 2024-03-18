65434debdf7be7ecfe5f71ffc354c0e9712d5a14
Thiago Lahass <lahassthiagofelippe@gmail.com>
2024-03-18 11:08:21 -0300

    Identifing range colors

--- color.py
+++ color.py
@@ -54,13 +54,13 @@ while(1):
 			cv2.putText(img,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
 			
 	#Tracking the Blue Color
-	# (contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-	# for pic, contour in enumerate(contours):
-	# 	area = cv2.contourArea(contour)
-	# 	if(area>300):
-	# 		x,y,w,h = cv2.boundingRect(contour)	
-	# 		img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-	# 		cv2.putText(img,"Blue color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
+	(contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if(area>300):
+			x,y,w,h = cv2.boundingRect(contour)	
+			img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+			cv2.putText(img,"Blue color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
 
 	#Tracking the yellow Color
 	(contours,hierarchy)=cv2.findContours(yellow,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
