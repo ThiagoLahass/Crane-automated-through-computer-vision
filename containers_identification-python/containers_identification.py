281c4d6cc2b2c0823a85846fed3e615c8e4e1d78
Thiago Lahass <lahassthiagofelippe@gmail.com>
2024-05-23 17:42:31 -0300

    :sparkles:adding possibility to choose different quantities of different colors

--- containers_identification-python/containers_identification.py
+++ containers_identification-python/containers_identification.py
@@ -49,10 +49,10 @@ V_MAX = 255
 # ============= TEST BEFORE USE ==========
 # initial min and max HSV filter to RED
 H_MIN_RED = 0
-H_MAX_RED = 16
-S_MIN_RED = 11
-S_MAX_RED = 85
-V_MIN_RED = 251
+H_MAX_RED = 176
+S_MIN_RED = 99
+S_MAX_RED = 136
+V_MIN_RED = 181
 V_MAX_RED = 255
 
 # initial min and max HSV filter to GREEN
@@ -64,20 +64,20 @@ V_MIN_GREEN = 121
 V_MAX_GREEN = 240
 
 # initial min and max HSV filter to BLUE
-H_MIN_BLUE = 60
-H_MAX_BLUE = 106
-S_MIN_BLUE = 25
-S_MAX_BLUE = 221
-V_MIN_BLUE = 249
+H_MIN_BLUE = 97
+H_MAX_BLUE = 108
+S_MIN_BLUE = 144
+S_MAX_BLUE = 247
+V_MIN_BLUE = 124
 V_MAX_BLUE = 255
 
 # initial min and max HSV filter to YELLOW
-H_MIN_YELLOW = 29
-H_MAX_YELLOW = 43
-S_MIN_YELLOW = 0
-S_MAX_YELLOW = 20
-V_MIN_YELLOW = 241
-V_MAX_YELLOW = 255
+H_MIN_YELLOW = 23
+H_MAX_YELLOW = 48
+S_MIN_YELLOW = 47
+S_MAX_YELLOW = 121
+V_MIN_YELLOW = 174
+V_MAX_YELLOW = 232
 # ============= END TEST BEFORE USE ==========
 
 RED     = 0
@@ -112,7 +112,7 @@ FRAME_HEIGHT    = 480
 MAX_NUM_OBJECTS = 5
 
 # minimum and maximum object area
-MIN_OBJECT_AREA = 20 * 20                                   # ADJUST WHEN DOING CONTAINER DETECTION ON CRANE
+MIN_OBJECT_AREA = 50 * 50                                   # ADJUST WHEN DOING CONTAINER DETECTION ON CRANE
 MAX_OBJECT_AREA = int(FRAME_HEIGHT * FRAME_WIDTH / 1.5)
 
 trackbar_window_name = "Trackbars"
@@ -221,7 +221,6 @@ def clean_noise_morph_ops(img):
 
     return img
 
-# use some of the openCV drawing functions to draw crosshairs on tracked image  
 def draw_crosshair(x, y, color, img):
     """
     Draws a crosshair on the image around the specified coordinates.
@@ -297,9 +296,38 @@ def get_center_of_nearest_container(img_mask, img_original, color):
     return my_point
 
 
-def select_color_of_containers():
+def select_colors_and_quantities():
     """
-    Prompt the user to select a color for the containers.
+    Prompt the user to select colors and quantities for containers.
+
+    Returns:
+        Tuple[List[int], List[int]]: Two lists, one containing the selected colors and the other containing the corresponding quantities.
+    """
+    colors = []
+    quantities = []
+
+    while True:
+        print("BACKEND: Quantos tipos diferentes de containers você deseja procurar?")
+        num_types = input("Digite o número de tipos: ").strip()
+
+        if num_types.isdigit() and int(num_types) > 0:
+            num_types = int(num_types)
+            break
+        else:
+            print("Entrada inválida. Por favor, insira um número inteiro maior que 0.")
+
+    for i in range(num_types):
+        print(f"Tipo de container {i + 1}:")
+        color = select_color()
+        quantity = select_quantity()
+        colors.append(color)
+        quantities.append(quantity)
+
+    return colors, quantities
+
+def select_color():
+    """
+    Prompt the user to select a color for a container type.
 
     Returns:
         int: Index corresponding to the selected color.
@@ -313,7 +341,7 @@ def select_color_of_containers():
         print("3 - YELLOW")
 
         color_container = input("Digite o número correspondente à cor: ").strip()
-        
+
         if color_container.isdigit():
             color_container = int(color_container)
             if color_container in range(4):
@@ -323,20 +351,20 @@ def select_color_of_containers():
         else:
             print("Entrada inválida. Por favor, insira um número.")
 
-def select_number_of_containers():
+def select_quantity():
     """
-    Prompt the user to select the number of containers to search.
+    Prompt the user to select the quantity of containers for a specific type.
 
     Returns:
-        int: Number of containers to search.
+        int: Quantity of containers.
     """
     while True:
-        number_containers = input("BACKEND: Quantos containers você deseja?\n").strip()
-        
-        if number_containers.isdigit():
-            number_containers = int(number_containers)
-            if number_containers > 0:
-                return number_containers
+        quantity = input("BACKEND: Quantos containers deste tipo você deseja?\n").strip()
+
+        if quantity.isdigit():
+            quantity = int(quantity)
+            if quantity > 0:
+                return quantity
             else:
                 print("Número inválido. Por favor, insira um número maior que 0.")
         else:
@@ -382,7 +410,7 @@ def main():
     # Read from webcam
     capture = cv2.VideoCapture(0)
 
-    # Abrindo logo as telas para nao termos delay depois
+    # Opening the screens immediately so we don't have delays later
     ret, img_original = capture.read()
     img_HSV = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
     img_mask = np.zeros((4, FRAME_HEIGHT, FRAME_WIDTH), dtype=np.uint8)
@@ -404,101 +432,111 @@ def main():
 
     # FIRST WHILE LOOP - IT'S REPRESENT EVERY TIME AS THE COMAND TO SEARCH A CONTAINER IS SEND
     while( not first_loop_flag ):
-  
-        number_containers = select_number_of_containers()
-        COLOR = select_color_of_containers()
         
-        for i in range(number_containers):
-            print(f"BACKEND: Buscando container {i+1} da cor {COLORS_NAME[COLOR]}")
+        # input of the container(s) information
+        colors, quantities = select_colors_and_quantities()
+        print("Cores selecionadas:", colors)
+        print("Quantidades selecionadas:", quantities)
 
-            #==================== ESP SERIAL COMUNICATION SENDING BEGIN COMAND ====================
-            # SERIAL COMUNICATION - READ
-            string = read_ser(port, MAX_BUFF_LEN)
-            print(string)
+        # for each set of containers of a color
+        for index, color in enumerate(colors):
+            # for each container
+            for i in range(quantities[index]):
+                print(f"BACKEND: Buscando container {i+1}/{quantities[index]} da cor {COLORS_NAME[color]}")
 
-            time.sleep(2)
+                #==================== ESP SERIAL COMUNICATION SENDING BEGIN COMAND ====================
+                # SERIAL COMUNICATION - READ
+                string = read_ser(port, MAX_BUFF_LEN)
+                print(string)
 
-            # SERIAL COMUNICATION - WRITE
-            cmd = 'begin'
-            write_ser(port, cmd)
+                # delay to ensure that ESP has time to read and respond to the Serial
+                time.sleep(1)
 
-            time.sleep(0.1)
+                # SERIAL COMUNICATION - WRITE
+                cmd = 'begin'
+                write_ser(port, cmd)
 
-            # SERIAL COMUNICATION - READ
-            string = read_ser(port, MAX_BUFF_LEN)
-            print(string)
-            #==================== END ESP SERIAL COMUNICATION SENDING BEGIN COMAND ====================
+                time.sleep(0.1)
 
-            # Reset the track control variable
-            track_objects = True
+                # SERIAL COMUNICATION - READ
+                string = read_ser(port, MAX_BUFF_LEN)
+                print(string)
+                #==================== END ESP SERIAL COMUNICATION SENDING BEGIN COMAND ====================
 
-            # SECOND WHILE LOOP - IT'S REPRESENT THE SEARCH FOR THE SELECTED CONTAINER
-            while True:
-                # capture the actual frame from the webcam
-                ret, img_original = capture.read()
-                if not ret:
-                    break
-                
-                # convert the BGR spacecolor to HSV to be easy to apply filters
-                img_HSV = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
+                # Reset the track control variable
+                track_objects = True
 
-                # CREATE A MASK FOR EVERY TRACKED COLOR
-                for i, thresholds in enumerate(my_colors_thresholds):
-                    lower = np.array(thresholds[:3])
-                    upper = np.array(thresholds[3:])
-                    img_mask[i] = cv2.inRange(img_HSV, lower, upper)
-                    if use_morph_ops:
-                        img_mask[i] = clean_noise_morph_ops(img_mask[i])
-                    cv2.imshow(COLORS_NAME[i], img_mask[i])
-
-                # Passagem do frame thresholded para nossa função de rastreamento de objetos
-                # esta função retornará as coordenadas x e y do objeto filtrado
-                if track_objects:
-                    # (x,y) = track_filtered_object(GREEN, img_mask, img_original)
-                    (x,y) = get_center_of_nearest_container(img_mask[COLOR], img_original, COLOR)
-                    if x != 0 and y != 0 :
-                        draw_crosshair(x, y, COLOR, img_original)
-                    delta_x = int (X_CENTER - x)
-                    delta_y = int (Y_CENTER - y)
-
-                    # Preenchimento com zeros à esquerda para garantir 3 dígitos
-                    delta_x_str = str(delta_x).zfill(4)
-                    delta_y_str = str(delta_y).zfill(4)
-
-                    # SERIAL COMUNICATION - WRITE
-                    cmd = f'{delta_x_str} {delta_y_str}'
-                    write_ser(port, cmd)
-
-                    time.sleep(0.1)
-
-                    # SERIAL COMUNICATION - READ
-                    string = read_ser(port, MAX_BUFF_LEN)
-                    if(len(string)):
-                        print(f"{string}")
-                        if (string == "ESP: container centralizado"):
-                            print("BACKEND: Parando de rastrear container...")
-                            track_objects = False
-
-                else:
-                    string = read_ser(port, MAX_BUFF_LEN)
-                    if(len(string)):
-                        print(f"{string}")
-                        if(string == "ESP: end"):
-                            print("BACKEND: Buscando proximo container, caso for o ultimo volta para o primeiro loop ('begin')")
-                            break
+                # SECOND WHILE LOOP - IT'S REPRESENT THE SEARCH FOR THE SELECTED CONTAINER
+                while True:
+                    # capture the actual frame from the webcam
+                    ret, img_original = capture.read()
+                    if not ret:
+                        break
                     
+                    # convert the BGR spacecolor to HSV, to be easy to apply filters
+                    img_HSV = cv2.cvtColor(img_original, cv2.COLOR_BGR2HSV)
 
-                # Exibição das imagens
-                cv2.imshow("Image Original", img_original)
-                cv2.imshow("Image HSV", img_HSV)
-                # cv2.imshow("Image Threshold", img_mask)
+                    # Create a mask for every tracked color, and show them
+                    for i, thresholds in enumerate(my_colors_thresholds):
+                        lower = np.array(thresholds[:3])
+                        upper = np.array(thresholds[3:])
+                        img_mask[i] = cv2.inRange(img_HSV, lower, upper)
+                        if use_morph_ops:
+                            img_mask[i] = clean_noise_morph_ops(img_mask[i])
+                        cv2.imshow(COLORS_NAME[i], img_mask[i])
 
-                key = cv2.waitKey(1)
-                if key == 27:  # Press ESC to exit
-                    first_loop_flag = True
-                    break
+                    if track_objects:
+                        # get the center of nearest container tracked and draw crosshairs above it 
+                        (x,y) = get_center_of_nearest_container(img_mask[color], img_original, color)
+                        if x != 0 and y != 0 :
+                            draw_crosshair(x, y, color, img_original)
+                        
+                        # calculates the difference between the container's current position and the center of the image
+                        delta_x = int (X_CENTER - x)
+                        delta_y = int (Y_CENTER - y)
 
-    # capture.release()
+                        # Padding with leading zeros to guarantee 4 digits (with sign)
+                        delta_x_str = str(delta_x).zfill(4)
+                        delta_y_str = str(delta_y).zfill(4)
+
+                        # SERIAL COMUNICATION - WRITE
+                        cmd = f'{delta_x_str} {delta_y_str}'
+                        write_ser(port, cmd)
+
+                        time.sleep(0.1)
+
+                        # SERIAL COMUNICATION - READ
+                        string = read_ser(port, MAX_BUFF_LEN)
+                        if(len(string)):
+                            print(f"{string}")
+                            # If the difference between the current position of the object in the camera
+                            # and the center is within the allowed limit, ESP sends a message to our BACKEND informing this
+                            if (string == "ESP: container centralizado"):
+                                print("BACKEND: Parando de rastrear container...")
+                                track_objects = False
+
+                    else:
+                        string = read_ser(port, MAX_BUFF_LEN)
+                        if(len(string)):
+                            print(f"{string}")
+                            # When the bridge has completed the search cycle for a container,
+                            # the ESP also sends a message to the BACKEND informing this fact
+                            if(string == "ESP: end"):
+                                print("BACKEND: Buscando proximo container, caso for o ultimo volta para o primeiro loop ('begin')")
+                                break
+                        
+
+                    # Display of original and HSV images
+                    cv2.imshow("Image Original", img_original)
+                    cv2.imshow("Image HSV", img_HSV)
+
+                    key = cv2.waitKey(1)
+                    # Press ESC to exit
+                    if key == 27:
+                        first_loop_flag = True
+                        break
+
+    capture.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
